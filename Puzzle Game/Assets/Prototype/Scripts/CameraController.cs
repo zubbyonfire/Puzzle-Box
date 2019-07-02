ffc05a3bb9a17652cdb90655aa523fbd45156172
Zack <zackbloundele@outlook.com>
2019-07-02 21:45:29 +0100

    Player prototype made with simple platforming mechanics
    Created a simple player prototype, with basic platforming mechanics - walk & jump. 
    Also added the logic for camera rotation & limits on the up/down.

---
+++ Puzzle Game/Assets/Prototype/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Camera Values")]
+    [SerializeField]
+    private float sensitivity;
+    [SerializeField]
+    private float smoothing;
+
+    private Vector2 mouseLook = Vector2.zero;
+    private Vector2 smoothV = Vector2.zero;
+
+    private GameObject playerObj = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get playerObj
+        playerObj = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseDir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDir.x *= sensitivity * smoothing;
+        mouseDir.y *= sensitivity * smoothing;
+
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDir.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDir.y, 1f / smoothing);
+
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        playerObj.transform.rotation = Quaternion.AngleAxis(mouseLook.x, playerObj.transform.up);
+    }
+}
---
+++ Puzzle Game/Assets/Prototype/Scripts/PlayerCharacterLogic.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// What this script does:
+/// Handles the player logic: Movement, Crouching, Jumping, Running
+/// </summary>
+public class PlayerCharacterLogic : MonoBehaviour
+{
+    [Header("Player Variables")]
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float jumpForce;
+
+    private Rigidbody rb = null;
+    private Vector3 userInput = Vector3.zero;
+    private Vector3 moveDir = Vector3.zero;
+
+    //Checks & Flag Variables
+    [Header("Ground Check Variables")]
+    [SerializeField]
+    private float groundDist;
+    [SerializeField]
+    private bool isGrounded = true;
+    [SerializeField]
+    private Transform groundCheck = null;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if player isGrounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundLayer, QueryTriggerInteraction.Ignore);
+
+        //Check for jump input
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+    /// <summary>
+    /// Handle getting the player input & jumping
+    /// </summary>
+    private void Movement()
+    {
+        userInput = Vector3.zero;
+        userInput.x = Input.GetAxisRaw("Horizontal");
+        userInput.z = Input.GetAxisRaw("Vertical");
+
+        moveDir = (userInput.z * transform.forward) + (userInput.x * transform.right);
+        moveDir = moveDir.normalized * movementSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + moveDir);
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+}
